bcae419ca9f4d7ae6f3b9d98c74b3aecc8f7a929
JAntonioCaballero <110413770+JAntonioCaballero@users.noreply.github.com>
2023-01-17 22:07:17 -0600

    HastaProps

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,11 +1,37 @@
 
-export const FirstApp = () => {
+export const FirstApp = (props) => {
+
+  const nuevoValor='Usuario';
+  const nuevoArreglo=[1,2,3,4,5,6,7,8,9,0];
+  const nuevaColeccion={
+    mensaje:'Hola',
+    nombre:'Usuario'
+  };
+
+  const unaFuncion=()=>{
+    return 5*3;
+  }
+
   return (
-    <img
-    src="https://i.imgur.com/yXOvdOSs.jpg"
-    alt="Cristopher Velazquez"
-    className="photo"
-    />
+    <>
+      <img
+      src="https://i.imgur.com/yXOvdOSs.jpg"
+      alt="Cristopher Velazquez"
+      className="photo"
+      />
+      <h1>{1+1}</h1>
+
+      <h1>Hola {nuevoValor}</h1>
+
+      <h1>Arreglo: {nuevoArreglo}</h1>
+
+      <h1>Coleccion: {JSON.stringify(nuevaColeccion)}</h1>
+
+      <h1>Funcion: {unaFuncion()}</h1>
+
+      {console.log(props)}
+
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { HelloWorlApp } from './HelloWorlApp';
 
-
-
-
+import './styles.css';
 
 ReactDOM.createRoot (document.getElementById('root')).render (
   <React.StrictMode>
     <HelloWorlApp/>
-    <FirstApp/>
-    <FirstApp/>
     <FirstApp/>
   </React.StrictMode>
 );
