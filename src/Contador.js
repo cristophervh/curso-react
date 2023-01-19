6f8b2f768b7d50d185967b6ec470b95c6f5eaec7
JAntonioCaballero <110413770+JAntonioCaballero@users.noreply.github.com>
2023-01-19 10:05:54 -0600

    Botones

--- src/Contador.js
+++ src/Contador.js
@@ -1,7 +1,50 @@
-export const Contador=({value})=>{
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const Contador=()=>{
+
+    const [valor,setCounter] =useState(0);
+
+    /*
+function handleAdd(event, nuevoValor){
+    console.log(event);
+    console.log(nuevoValor);
+    valor=1000;
+}
+*/
+
+const handleAdd=()=>{
+    setCounter(valor+1)
+}
+
+const handleSubs=()=>{
+    setCounter(valor-1)
+}
+
+const handleReset=()=>{
+    setCounter(0)
+}
+
     return(
         <>
-        <h1>Valor: {value}</h1>
+        <h1>Valor: {valor}</h1>
+
+        <button onClick={(event) => handleAdd()} >
+            +1
+        </button>
+
+        <button onClick={(event) => handleSubs()} >
+            -1
+        </button>
+
+        <button onClick={(event) => handleReset()} >
+            Reset
+        </button>
+
         </>
     )
+}
+
+Contador.propTypes={
+    valor:PropTypes.number.isRequired
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
-import { HelloWorlApp } from './HelloWorlApp';
+
 import { Contador } from './Contador';
 
 import './styles.css';
 
 ReactDOM.createRoot (document.getElementById('root')).render (
   <React.StrictMode>
-    <HelloWorlApp/>
-    <FirstApp titulo="Hola Prop" />
+    
     <Contador valor={10} />
   </React.StrictMode>
 );
